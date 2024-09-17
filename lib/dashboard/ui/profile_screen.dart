67335e0b62f7e39546189fcf91bf9dbfb311b6b2
smith <smitha.r@appisode.in>
2024-09-17 14:48:27 +0530

    bug fixes

--- lib/dashboard/ui/profile_screen.dart
+++ lib/dashboard/ui/profile_screen.dart
@@ -629,7 +629,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                           height: 20,
                         ),
                         const Text(
-                          "V 1.1.7",
+                          "V 1.1.8",
                           style: TextStyle(color: Colors.grey),
                         ),
                         const SizedBox(
